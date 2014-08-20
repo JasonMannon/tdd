3a567917aa2b2aa9402893baba60016e365e1048
Jason Mannon <jmannonpdx@gmail.com>
2014-08-20 11:58:37 -0700

    adds person#merge and person#duplicates? methods to make specs pass

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,5 +1,21 @@
 class Person < ActiveRecord::Base
-  attr_accessible :first_name, :last_name, :comapny_name, :email
-  
+  attr_accessible :first_name, :last_name, :company_name, :email
   has_many :notes
+
+  def self.merge(winner, loser)
+    if winner.email.blank?
+      winner.email = loser.email 
+    end
+    winner.notes << loser.notes
+    loser.delete
+    winner
+  end
+
+  def duplicates?(person)
+    if person
+      self.first_name == person.first_name && 
+      self.last_name == person.last_name && 
+      self.email == person.email
+    end
+  end
 end
